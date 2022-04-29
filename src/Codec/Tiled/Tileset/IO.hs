43362d39dfcacd14f773d75076400379398d3ef1
IC Rainbow <aenor.realm@gmail.com>
2022-04-30 00:10:56 +0300

    Add more IO

--- src/Codec/Tiled/Tileset/IO.hs
+++ src/Codec/Tiled/Tileset/IO.hs
@@ -1,5 +1,5 @@
-module Codec.Tiled.Map.IO
-  ( MapError(..)
+module Codec.Tiled.Tileset.IO
+  ( TilesetError(..)
   , readFile
   , writeFile
   ) where
@@ -14,24 +14,24 @@ import Data.ByteString qualified as ByteString
 import Data.Text (Text)
 import Data.Text qualified as Text
 
-import Codec.Tiled.Map (Map)
+import Codec.Tiled.Tileset (Tileset)
 
-newtype MapError = MapError Text
+newtype TilesetError = TilesetError Text
   deriving (Eq, Show)
 
-instance Exception MapError
+instance Exception TilesetError
 
-readFile :: MonadIO m => FilePath -> m Map
+readFile :: MonadIO m => FilePath -> m Tileset
 readFile source = liftIO do
   bytes <- ByteString.readFile source
   case decodeMap bytes of
     Left msg ->
-      throwIO $ MapError (Text.pack msg)
+      throwIO $ TilesetError (Text.pack msg)
     Right res ->
       pure res
 
-decodeMap :: ByteString -> Either String Map
+decodeMap :: ByteString -> Either String Tileset
 decodeMap = Aeson.eitherDecodeStrict'
 
-writeFile :: MonadIO m => FilePath -> Map -> m ()
+writeFile :: MonadIO m => FilePath -> Tileset -> m ()
 writeFile destination = liftIO . Aeson.encodeFile destination
